0fb7b637ee11ee3d44adeb10778ae572dd30ab77
atodorov284 <atodorov284@gmail.com>
2024-09-06 13:56:50 +0200

    Deleted files and initial setup

--- air-quality-forecast/__init__.py
+++ air-quality-forecast/__init__.py
@@ -1,15 +1,3 @@
 import numpy as np
 
-import random
-import sys
-from dvclive import Live
-
-with Live(save_dvc_exp=True) as live:
-    epochs = 3
-    live.log_param("epochs", epochs)
-    for epoch in range(epochs):
-        live.log_metric("train/accuracy", epoch + random.random())
-        live.log_metric("train/loss", epochs - epoch - random.random())
-        live.log_metric("val/accuracy",epoch + random.random() )
-        live.log_metric("val/loss", epochs - epoch - random.random())
-        live.next_step()
+print(np.array(1))
