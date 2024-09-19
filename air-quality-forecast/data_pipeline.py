8a619cffdcb348a3342352622efdd985f5565c8d
LukaszSawala <lukaszsawala2003@gmail.com>
2024-09-19 22:10:26 +0200

    pipeline preprocessing steps improvement/expansion

--- air-quality-forecast/data_pipeline.py
+++ air-quality-forecast/data_pipeline.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 
 from utils import FeatureSelector
-from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.model_selection import train_test_split
 
 class PreprocessingPipeline:
     def __init__(self):
@@ -86,21 +86,26 @@ class PreprocessingPipeline:
         
         #Add domain knowledge columns
         domain_knowledge_columns = ['precip','windspeed', 'winddir']
-        selected_columns = selected_columns + domain_knowledge_columns
+        selected_columns = ['date'] + selected_columns + domain_knowledge_columns
 
         return data[selected_columns]
         
-    def apply_time_shift(self, data, t = 3):
+    def apply_time_shift(self, data, t_max = 3):
         """ 
         Applies the time shift to the dataset and adds the shifted columns.
+
+        :param data: Raw data as a Pandas DataFrame
+        :param t_max: Maximum time shift
+        :return: Data with time shifted columns as a Pandas DataFrame
         """
-        all_cols = data.columns
-        
-        for t in range(1,t+1):
+        all_cols = data.columns 
+        # Apply time shift to the predictors
+        for t in range(1,t_max+1):
             for col in all_cols:
                 data[[f'{col} - day {t}']] = data[[col]].shift(-t)
 
-        for t in range(1,t):
+        # Apply time shift to the labels
+        for t in range(1,t_max):
             for col in ['o3', 'no2']:
                 data[[f'{col} + day {t}']] = data[[col]].shift(t)
 
@@ -114,17 +119,67 @@ class PreprocessingPipeline:
         :param raw_data: Raw data as a Pandas DataFrame
         :return: Preprocessed data as a Pandas DataFrame
         """
+        # Select the relevant features
         data = self.select_features(data)
+
+        # Set date as index
+        data.set_index('date', inplace=True)
+
+        # Remove missing values
+        data = data.dropna(subset=['o3','no2'])
+
+        # Apply time shift
         data = self.apply_time_shift(data)
+
+        # Remove unnecessary columns and rows
         data.drop(['pm25','pm10','temp','humidity','visibility','solarradiation','precip','windspeed','winddir'], axis=1, inplace=True)
         data.drop(index=['29/01/2014','30/01/2014','31/01/2014', '10/09/2024', '11/09/2024'], inplace=True)
-    
+        
         return data
     
-    def normalize_data(self, data, normalizer_type = ""):
-        if normalizer_type == "":
+    def normalize_data(self, data, normalizer_type = "default"):
+        """
+        Normalize the data using the specified normalizer.
+
+        :param data: Data to be normalized as a Pandas DataFrame
+        :param normalizer_type: Type of normalizer to use. Options are 'default', 'MinMax', 'Standard', and 'Robust'.
+        :return: Normalized data as a Pandas DataFrame
+        """
+        if normalizer_type == "default":
+            print("No normalizer selected. Using default MinMaxScaler.")
             ss = MinMaxScaler()
+        elif normalizer_type == "MinMax" or normalizer_type == "minmax" or \
+            normalizer_type == "MinMaxScaler" or normalizer_type == "minmaxscaler":    
+            ss = MinMaxScaler()
+        elif normalizer_type == "Standard" or normalizer_type == "standard" or \
+            normalizer_type == "StandardScaler" or normalizer_type == "standardscaler":
+            ss = StandardScaler()
+        elif normalizer_type == "Robust" or normalizer_type == "robust" or \
+            normalizer_type == "RobustScaler" or normalizer_type == "robustscaler":
+            ss = RobustScaler()
+        else:
+            print("Wrong normalizer name. Using default MinMaxScaler.")
+            ss = MinMaxScaler()
+
+        return ss.fit_transform(data)
         
+    
+    def train_test_split(self, data, test_size_ = 0.2):
+        """
+        Split the data into training and testing sets.
+
+        :param data: Data to be split as a Pandas DataFrame
+        :param test_size: Percentage of the data to be used for testing
+        :return: Training and testing sets as Pandas DataFrames
+        """
+        columns_to_predict = ['no2', 'o3', 'no2 + day 1', 'o3 + day 1', 'no2 + day 2', 'o3 + day 2']
+        # Remove labels from the data
+        x = data.drop(columns_to_predict, axis=1)
+
+        # Select the labels
+        y = data[columns_to_predict]
+        
+        return train_test_split(x, y, test_size=test_size_, random_state=42)
 
     def save_to_csv(self, name, data, path):
         """
