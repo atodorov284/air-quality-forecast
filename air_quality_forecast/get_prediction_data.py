50326b8b8bddc74822f3d48e086b2a06f82875d4
atodorov284 <atodorov284@gmail.com>
2024-10-18 16:29:08 +0200

    added a datatable to add each day prediction

--- air_quality_forecast/get_prediction_data.py
+++ air_quality_forecast/get_prediction_data.py
@@ -11,21 +11,43 @@ def main():
     sys.path.append(parent_dir)
 
     predictor = PredictorModels()
-
-    project_root = os.path.dirname(os.path.dirname(__file__))
-    parent_dir = os.path.dirname(project_root)
-    sys.path.append(parent_dir)
-
     caller = APICaller()
-    predictor = PredictorModels()
-    current_data = caller.lag_data()
-    df = pd.DataFrame(predictor.xgb_predictions(current_data))
-    df.columns = ["NO2", "O3", "NO2 + day 1", "O3 + day 1", "NO2 + day 2", "O3 + day 2"]
+    all_predictions = caller.lag_data()
 
-    save_dir = os.path.join(project_root, "data", "inference")
+    df = pd.DataFrame(predictor.xgb_predictions(all_predictions))
+    df.columns = [
+        "NO2 + day 1",
+        "O3 + day 1",
+        "NO2 + day 2",
+        "O3 + day 2",
+        "NO2 + day 3",
+        "O3 + day 3",
+    ]
+    current_date = pd.Timestamp.today().strftime("%d-%m-%Y")
+    df.index = pd.DatetimeIndex([current_date])
 
-    pd.DataFrame(current_data).to_csv(os.path.join(save_dir, "last_three_days.csv"))
-    df.to_csv(os.path.join(save_dir, "current_prediction_data.csv"))
+    save_dir = os.path.join(project_root, "data", "model_predictions")
+
+    pd.DataFrame(all_predictions).to_csv(os.path.join(save_dir, "last_three_days.csv"))
+
+    try:
+        all_predictions = pd.read_csv(
+            os.path.join(save_dir, "prediction_data.csv"),
+            index_col="date",
+            parse_dates=True,
+        )
+    except FileNotFoundError:
+        all_predictions = pd.DataFrame(columns=df.columns, index=pd.DatetimeIndex([]))
+
+    if current_date in all_predictions.index:
+        all_predictions.loc[current_date] = df.loc[current_date]
+    else:
+        # Append the new row using pd.concat
+        all_predictions = pd.concat([all_predictions, df])
+
+    all_predictions.to_csv(
+        os.path.join(save_dir, "prediction_data.csv"), index_label="date"
+    )
 
 
 if __name__ == "__main__":
