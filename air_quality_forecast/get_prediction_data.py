26d29d762c738893ebb68c438c91130d60531ce1
atodorov284 <atodorov284@gmail.com>
2024-10-18 14:25:31 +0200

    add all functionality for api data fetch and predict

--- air_quality_forecast/get_prediction_data.py
+++ air_quality_forecast/get_prediction_data.py
@@ -1,4 +1,6 @@
 from prediction import PredictorModels
+from api_caller import APICaller
+import pandas as pd
 import os
 import sys
 
@@ -10,26 +12,20 @@ def main():
 
     predictor = PredictorModels()
 
-    predictor.xgb_predictions()
-
-    # project_root = os.path.dirname(os.path.dirname(__file__))
-    # parent_dir = os.path.dirname(project_root)
-    # sys.path.append(parent_dir)
-
-    # caller = APICaller()
-    # predictor = PredictorModels()
-    # current_data = caller.lag_data()
-    # df = pd.DataFrame(predictor.xgb_predictions(current_data))
-    # df.columns = ["NO2", "O3", "NO2 + day 1", "O3 + day 1", "NO2 + day 2", "O3 + day 2"]
-
-    # save_dir = os.path.join(project_root, "data", "inference")
-
-    # df.to_csv(os.path.join(save_dir, "current_prediction_data.csv"))
-
-
-if __name__ == "__main__":
     project_root = os.path.dirname(os.path.dirname(__file__))
     parent_dir = os.path.dirname(project_root)
     sys.path.append(parent_dir)
 
+    caller = APICaller()
     predictor = PredictorModels()
+    current_data = caller.lag_data()
+    df = pd.DataFrame(predictor.xgb_predictions(current_data))
+    df.columns = ["NO2", "O3", "NO2 + day 1", "O3 + day 1", "NO2 + day 2", "O3 + day 2"]
+
+    save_dir = os.path.join(project_root, "data", "inference")
+
+    df.to_csv(os.path.join(save_dir, "current_prediction_data.csv"))
+
+
+if __name__ == "__main__":
+    main()
