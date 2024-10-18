f60088738fda88bcbd67133c49b7349193c93137
atodorov284 <atodorov284@gmail.com>
2024-10-18 20:09:49 +0200

    admin dashboard for custom predict

--- streamlit_src/controllers/admin_controller.py
+++ streamlit_src/controllers/admin_controller.py
@@ -1,16 +1,116 @@
 from views.admin_view import AdminView
 import streamlit as st
 from controllers.user_controller import UserController
+import pandas as pd
+from air_quality_forecast.prediction import PredictorModels
 
 
 class AdminController(UserController):
-    def __init__(self):
+    """
+    A class to handle the admin interface.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the AdminController class.
+        """
         super().__init__()
         self._view = AdminView()
 
-    def show_dashboard(self):
+    def show_dashboard(self) -> None:
+        """
+        Shows the main page of the admin interface.
+        """
         st.markdown("ADMIN DASHBOARD")
-        super().show_dashboard()
+        self._show_current_data()
+        self._display_plots()
+        self._make_custom_predictions()
 
-    def welcome_back(self):
+    def welcome_back(self) -> None:
+        """
+        Shows a welcome message for the admin interface.
+        """
         self._view.welcome_back()
+
+    def _compute_metrics(self) -> None:
+        """
+        Computes the metrics for the admin interface.
+        """
+        pass
+
+    def _make_custom_predictions(self) -> None:
+        """
+        Makes a custom prediction for the admin interface.
+        """
+        self._view.upload_instructions()
+        checks = {
+            "The data must be unnormalized.": False,
+            "PM25, PM10, O3, NO2 should be in micrograms per cubic meter (µg/m³).": False,
+            "Temperature should be in degrees Celcius": False,
+            "Add the others later": False,
+            "The dataset must contain a total of 33 columns in the specified order.": False,
+            "I accept that my data will be used for a prediction using a custom model.": False,
+            "I understand that my data will not be saved.": False,
+        }
+
+        all_checks_marked = self._view.confirm_checks(checks)
+
+        if all_checks_marked:
+            dataset = self._view.upload_dataset()
+            if dataset is not None:
+                data = pd.read_csv(dataset)
+                self._perform_data_validation(data)
+                if "date" in data.columns or "datetime" in data.columns:
+                    data.set_index(
+                        "date" if "date" in data.columns else "datetime", inplace=True
+                    )
+                self._view.display_datatable(data, message="### User Data")
+
+                prediction = self._make_prediction(data)
+                self._view.display_datatable(
+                    prediction, message="### XGBoost Model Prediction (first 6 rows)"
+                )
+                self._view.download_button(
+                    label="Download predictions as CSV",
+                    data=prediction.to_csv(index=True),
+                    file_name="predictions.csv",
+                )
+
+        else:
+            st.warning("Please confirm all the requirements by marking the checkboxes.")
+
+    def _make_prediction(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Makes a prediction using an XGBoost model.
+
+        Args:
+            data (pd.DataFrame): The data to make the prediction on.
+
+        Returns:
+            pd.DataFrame: The prediction.
+        """
+        predictor = PredictorModels()
+        prediction = predictor.xgb_predictions(data)
+        prediction = pd.DataFrame(
+            prediction,
+            columns=[
+                "NO2 + day 1",
+                "O3 + day 1",
+                "NO2 + day 2",
+                "O3 + day 2",
+                "NO2 + day 3",
+                "O3 + day 3",
+            ],
+            index=data.index,
+        )
+
+        return prediction
+
+    def _perform_data_validation(self, data: pd.DataFrame) -> None:
+        """
+        Performs data validation on the user data.
+
+        Args:
+            data (pd.DataFrame): The user data.
+        """
+        pass
--- streamlit_src/views/admin_view.py
+++ streamlit_src/views/admin_view.py
@@ -1,13 +1,120 @@
 # views/user_view.py
+from typing import Dict
 import streamlit as st
 from views.user_view import UserView
+import pandas as pd
 
 
 class AdminView(UserView):
-    def __init__(self):
+    """
+    A class to handle all user interface elements for the admin.
+
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the AdminView class.
+        """
         super().__init__()
 
-    def welcome_back(self):
+    def welcome_back(self) -> None:
+        """
+        Welcomes the user back to the admin page.
+        """
         st.empty()
         st.success("Welcome back!")
         st.empty()
+
+    def upload_dataset(self) -> pd.DataFrame:
+        """
+        Uploads a dataset for prediction.
+
+        :return: A pandas DataFrame containing the uploaded dataset.
+        """
+        return st.file_uploader("Choose a CSV file", type=["csv"])
+
+    def upload_instructions(self) -> None:
+        """
+        Displays instructions for uploading a dataset.
+        """
+        st.subheader("Upload Dataset for Prediction")
+        st.markdown(
+            "Make your own custom predictions using a dataset. Note that the dataset has to adhere to the following:"
+        )
+        st.markdown("""
+    - **Normalization**: The data must be **unnormalized**.
+    - **Units**: All measurements should be in **micrograms/m³**.
+    - **Data Structure**: The dataset must contain a total of **33 columns** with the following order:
+        - **Optional**: `date`
+        - `PM25` (two days ago)
+        - `PM10` (two days ago)
+        - `O3` (two days ago)
+        - `NO2` (two days ago)
+        - `Temperature` (two days ago)
+        - `Humidity` (two days ago)
+        - `Visibility` (two days ago)
+        - `Solar Radiation` (two days ago)
+        - `Precipitation` (two days ago)
+        - `Wind Speed` (two days ago)
+        - `Wind Direction` (two days ago)
+        - `PM25` (yesterday)
+        - `PM10` (yesterday)
+        - `O3` (yesterday)
+        - `NO2` (yesterday)
+        - `Temperature` (yesterday)
+        - `Humidity` (yesterday)
+        - `Visibility` (yesterday)
+        - `Solar Radiation` (yesterday)
+        - `Precipitation` (yesterday)
+        - `Wind Speed` (yesterday)
+        - `Wind Direction` (yesterday)
+        - `PM25` (today)
+        - `PM10` (today)
+        - `O3` (today)
+        - `NO2` (today)
+        - `Temperature` (today)
+        - `Humidity` (today)
+        - `Visibility` (today)
+        - `Solar Radiation` (today)
+        - `Precipitation` (today)
+        - `Wind Speed` (today)
+        - `Wind Direction` (today)
+    """)
+
+    def download_button(self, label: str, data: str, file_name: str) -> None:
+        """
+        Creates a download button for the user to download the data.
+
+        :param label: The label of the button.
+        :param data: The data to be downloaded.
+        :param file_name: The name of the file to be downloaded.
+        """
+        st.download_button(
+            label=label,
+            data=data,
+            file_name=file_name,
+            mime="csv",
+        )
+
+    def confirm_checks(self, checks: Dict[str, bool]) -> bool:
+        """
+        Displays a confirmation checkbox for the user to confirm the requirements.
+
+        :param checks: A dictionary of requirements and their current values.
+        :return: True if all the requirements are confirmed, False otherwise.
+        """
+        st.markdown("Select the requirements that have been checked:")
+        for requirement in checks.keys():
+            checks[requirement] = st.checkbox(requirement, value=checks[requirement])
+
+        return all(checks.values())
+
+    def display_datatable(self, data: pd.DataFrame, message: str) -> None:
+        """
+        Displays a table of the data.
+
+        :param data: The data to be displayed.
+        :param message: The message to be displayed above the table.
+        """
+        st.markdown(message)
+        st.dataframe(data.head())
